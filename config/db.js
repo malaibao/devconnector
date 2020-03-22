9f60a076e0dd7b119b958b3f7227e05956b76893
Maribelle <maribelleleongzz@gmail.com>
2020-03-22 18:09:09 -0300

    completed users and auth(register and login) back-end

---
+++ config/db.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI');
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true
+    });
+    console.log('DB is connected....');
+  } catch (err) {
+    console.log(err.message);
+    process.exit(1); //exit process with failure
+  }
+};
+
+module.exports = connectDB;
