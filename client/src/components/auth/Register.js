e0a33bdc15be59ce77219e01a7b8f8ba4f8e1d52
Maribelle <maribelleleongzz@gmail.com>
2020-03-25 22:01:32 -0300

    complete redux for auth

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import { setAlert } from '../../actions/alert';
+import { register } from '../../actions/auth';
 import PropTypes from 'prop-types';
 
-const Register = ({ setAlert }) => {
+const Register = ({ setAlert, register, isAuthenticated }) => {
   const [formData, setFormData] = useState({
     name: '',
     email: '',
@@ -23,9 +24,14 @@ const Register = ({ setAlert }) => {
       // props.setAlert('Passwords do not match', 'danger');
       setAlert('Passwords do not match', 'danger');
     } else {
-      console.log('success');
+      register({ name, email, password });
     }
   };
+
+  // REDIRECT TO DASHBOARD
+  if (isAuthenticated) {
+    return <Redirect to='/dashboard' />;
+  }
 
   return (
     <React.Fragment>
@@ -87,8 +93,14 @@ const Register = ({ setAlert }) => {
   );
 };
 
-Register.protoTypes = {
-  setAlert: PropTypes.func.isRequired
+Register.propTypes = {
+  setAlert: PropTypes.func.isRequired,
+  register: PropTypes.func.isRequired,
+  isAuthenticated: PropTypes.bool
 };
 
-export default connect(null, { setAlert })(Register);
+const mapStateToProps = state => ({
+  isAuthenticated: state.auth.isAuthenticated
+});
+
+export default connect(mapStateToProps, { setAlert, register })(Register);
---
+++ client/src/utils/setAuthToken.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+// ADDING GLOBAL HEADER
+const setAuthToken = token => {
+  if (token) {
+    axios.defaults.headers.common['x-auth-token'] = token;
+  } else {
+    delete axios.defaults.headers.common['x-auth-token'];
+  }
+};
+
+export default setAuthToken;
